2e2eea40288b54c5e3b97f526c390add9bed90bc
ArtsAnton <artsanton@yandex.ru>
2021-06-16 22:44:40 +0300

    Fixed func name - get_last_launch_links

--- api/les_4/spacex.py
+++ api/les_4/spacex.py
@@ -8,7 +8,7 @@ from utils import create_dir_for_img, download_img
 logger = logging.getLogger(__file__)
 
 
-def get_links_last_launch(url):
+def get_last_launch_links(url):
     """The function returns a list of links (links for image) from the last spacex launch."""
     response = requests.get(url)
     response.raise_for_status()
@@ -22,7 +22,7 @@ def main():
     img_dir = create_dir_for_img()
 
     try:
-        spacex_links = get_links_last_launch(spacex_url)
+        spacex_links = get_last_launch_links(spacex_url)
         for current_id, img_link in enumerate(spacex_links, start=1):
             img_title = f"spacex_{current_id}.jpg"
             download_img(img_dir, img_title, img_link)
