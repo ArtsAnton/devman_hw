253f797eba19133827144e66710c6e2ce21a5844
ArtsAnton <artsanton@yandex.ru>
2021-06-16 22:18:44 +0300

    Quotation marks are corrected.

--- api/les_4/utils.py
+++ api/les_4/utils.py
@@ -6,7 +6,7 @@ import requests
 def create_dir_for_img():
     """Create dir if not exists. Return path for images."""
     root = os.path.dirname(__file__)
-    new_path = os.path.join(root, 'image')
+    new_path = os.path.join(root, "image")
     os.makedirs(new_path, exist_ok=True)
     return new_path
 
@@ -15,5 +15,5 @@ def download_img(dir_name, img_title, url):
     """The function downloads images."""
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    with open(os.path.join(dir_name, img_title), 'wb') as file:
+    with open(os.path.join(dir_name, img_title), "wb") as file:
         file.write(response.content)
