cb2156a2e7059052ed69bee9ddebcccdcdcf41ec
ArtsAnton <artsanton@yandex.ru>
2021-06-22 21:40:18 +0300

    del drf

--- drf/les_1/project/utils.py
+++
@@ -1,5 +0,0 @@
-def format_duration(duration):
-    hours = duration // 3600
-    minutes = (duration % 3600) // 60
-    seconds = int((duration % 3600) % 60)
-    return f'{hours} ч. {minutes} мин. {seconds} сек.'
